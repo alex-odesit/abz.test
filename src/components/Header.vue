4b9f61506b5d4e65f80dca7722c4c5fa40a53c0a
Alex Dmitreiv <a.dmitriev@gamzix.com>
2024-03-02 11:11:05 +0200

    [header] created header

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <nav class="nav-bar">
+        <div class="header__image">
+          <img src="../assets/images/logo.svg" alt="logo" />
+        </div>
+
+        <div class="header__buttons">
+          <Button>Users</Button>
+          <Button>Sing up</Button>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Button from "@/components/ui/Button.vue";
+
+export default defineComponent({
+  name: "Header",
+  components: { Button },
+});
+</script>
+
+<style scoped lang="scss">
+.nav-bar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 13px 0;
+}
+
+.header__buttons {
+  display: flex;
+  align-items: center;
+  button {
+    margin-left: 10px;
+  }
+}
+</style>
---
+++ src/components/ui/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="$emit('buttonClick')" class="button"><slot></slot></button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Button",
+});
+</script>
+
+<style scoped lang="scss">
+.button {
+  padding: 4px 23px;
+  transition: all 0.3s ease;
+  background: #f4e041;
+  border-radius: 80px;
+  //cursor: pointer;
+  text-transform: none;
+  &:hover {
+    background: #ffe302;
+  }
+  &:disabled {
+    background: #b4b4b4;
+    color: #ffffffde;
+  }
+}
+</style>
